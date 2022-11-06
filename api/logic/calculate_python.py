c3b037e1e2a2f1fc7b83e94c5b7b8c95aed3a0a0
christianjensen2903 <cmj290300@gmail.com>
2022-11-06 02:21:36 +0100

    Assign variables and more in python code

--- api/logic/calculate_python.py
+++ api/logic/calculate_python.py
@@ -1,6 +1,36 @@
+import re
+from logic.SAVED_VARIABLES import SAVED_VARIABLES
+import traceback
 
 def calculate_python(code: str) -> str:
     """Calculate the result of a python block"""
-    return str(eval(code))
+
+    output = ""
+
+    # print = lambda x: f'{output}\n{x}' # override print function to append to output
+    def print(x):
+        nonlocal output
+        output += str(x) + '\n'
+
+    try:
+        for line in code.splitlines():
+            # Regex to check for variable assignment
+            assignment_re = re.compile(r"^(?P<variable>\w+)\s*=\s*(?P<expression>.+)$")
+            match = assignment_re.match(line)
+            if match:
+                variable = match.group('variable')
+                expression = match.group('expression')
+                SAVED_VARIABLES[variable] = eval(expression, SAVED_VARIABLES)
+            else:
+                # output += str(eval(line, SAVED_VARIABLES)) + '\n'
+                print(eval(line, SAVED_VARIABLES))
+            
+    except Exception as e:
+        print(traceback.format_exc())
+
+    return output
+
+
+    
 
 
