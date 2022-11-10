7c7a406aa0c213b916cae538d3e009f6ddce74d5
christianjensen2903 <cmj290300@gmail.com>
2022-11-10 17:38:33 +0100

    Send delta to server and change document.
    Bug: Frontend connects two times instead of only one.
    Missing to apply emit from server to other clients

--- api/main.py
+++ api/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.responses import HTMLResponse, FileResponse
 from html import html
 from logic import process
-from typing import List
+from typing import List, Dict, Any
 from fastapi.middleware.cors import CORSMiddleware
+import json
+import wordsmiths
 
 
 app = FastAPI()
@@ -21,18 +23,28 @@ app.add_middleware(
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: Dict(str, WebSocket) = {}
+        self.cursor_positions: Dict(str, int) = {}
+        self.content = ""
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, client_id: str, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[client_id] = websocket
+        self.cursor_positions[client_id] = 0
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
+    def disconnect(self, client_id: str):
+        del self.active_connections[client_id]
+        del self.cursor_positions[client_id]
+        
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
+        for connection in self.active_connections.values():
             await connection.send_text(message)
+
+    async def emit(self, message: str, client_id: str):
+        print(client_id)
+        for id, connection in self.active_connections.items():
+            if id != client_id:
+                await connection.send_text(message)
 
 
 manager = ConnectionManager()
@@ -49,16 +61,47 @@ async def load_notebook():
     return process.load_notebook()
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect(websocket)
+def insert (source_str: str, insert_str: str, pos: int):
+    """Inserts a string into a string at a given position"""
+    return source_str[:pos]+insert_str+source_str[pos:]
+
+def delete (source_str: str, num_char: int, pos: int):
+    """Deletes a n numbers of characters from a string at a given position"""
+    left = source_str[:max(pos-num_char,0)] # The current position minus the number of characters to delete
+    remaining = num_char - len(left) # The remaining number of characters to delete 
+    return left+source_str[pos+remaining:]
+
+
+def apply_delta(client_id: str, delta: dict[str, List[dict[str, Any]]]):
+    """Applies a delta to the content of the notebook"""
+    ops = delta["ops"]
+    for op in ops:
+
+        cursor_pos = manager.cursor_positions[client_id]
+        if "insert" in op:
+            manager.content = insert(manager.content, op["insert"], cursor_pos)
+        elif "delete" in op:
+            manager.content = delete(manager.content, op["delete"],cursor_pos)
+        elif "retain" in op:
+            manager.cursor_positions[client_id] = op["retain"]
+        
+
+    
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(client_id, websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(process.update(data))
+            data = json.loads(data)
+            apply_delta(client_id, data)
+            await manager.emit(manager.content, client_id)
+            # await manager.broadcast(process.update(data))
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{manager.active_connections.index(websocket)} left the chat")
+        manager.disconnect(client_id)
+        await manager.broadcast(f"Client #{client_id} left the notebook")
 
 
 @app.get("/images/{image_id}")
